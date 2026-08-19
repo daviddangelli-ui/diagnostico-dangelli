f12d151bcee0bdb7a52328c5c9b69f99a54a5252
daviddangelli-ui <david.dangelli@gmail.com>
2026-08-19 15:50:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.sidebar.divider()
 st.sidebar.caption("v 6.0 - DANGELLI Governance & Strategy")
 
 # --- CABEÇALHO PRINCIPAL ---
-st.title("🛡️ DANGELLI - Diagnóstico Estratégico Corporativo - Govenança/Estratégia/Finanças/Operações/Logística/RH")
+st.title("🛡️ DANGELLI - Diagnóstico Estratégico Corporativo 2026 - inclui Reforma Tributária")
 
 # Formulário de Identificação
 col_a, col_b = st.columns(2)
